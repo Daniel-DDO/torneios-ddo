89461379353c9102f77ac3483e604cade4248199
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-11-25 08:41:08 -0300

    Atualização

--- src/main/java/com/ddo/torneios/controller/TorneioController.java
+++ src/main/java/com/ddo/torneios/controller/TorneioController.java
@@ -1,5 +1,6 @@
 package com.ddo.torneios.controller;
 
+import com.ddo.torneios.service.TorneioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TorneioController {
 
     @Autowired
-    private TorneioController torneioController;
+    private TorneioService torneioService;
 
 }
