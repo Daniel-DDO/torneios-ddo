4b6a52be32509734442a043d28dace0d3120483f
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-12-15 07:19:12 -0300

    Atualização

--- src/main/java/com/ddo/torneios/controller/InsigniaController.java
+++ src/main/java/com/ddo/torneios/controller/InsigniaController.java
@@ -1,9 +1,16 @@
 package com.ddo.torneios.controller;
 
+import com.ddo.torneios.service.InsigniaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/insignia")
 public class InsigniaController {
+
+    @Autowired
+    private InsigniaService insigniaService;
+
+
 }
