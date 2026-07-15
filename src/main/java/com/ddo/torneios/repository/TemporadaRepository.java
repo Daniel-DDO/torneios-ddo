362f86fd9d7eef183e8d025b9e32467c692c5569
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2026-07-15 15:24:33 -0300

    Atualização das Temporadas

--- src/main/java/com/ddo/torneios/repository/TemporadaRepository.java
+++ src/main/java/com/ddo/torneios/repository/TemporadaRepository.java
@@ -2,6 +2,8 @@ package com.ddo.torneios.repository;
 
 import com.ddo.torneios.model.Temporada;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,5 @@ import java.util.Optional;
 public interface TemporadaRepository extends JpaRepository<Temporada, String> {
     boolean existsTemporadaByNome(@NotBlank String nome);
     Optional<Temporada> findByNome(String nome);
+    Page<Temporada> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 }
